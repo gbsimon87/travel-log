b6a80b65f44cb620a91ceba2409f4e29d642bf13
Simon Cordova <>
2020-06-27 10:59:37 +0100

    feat: add not found, error handling middlewares, and other middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const { notFound, errorHandler } = require("./middlewares");
 
 const app = express();
-
 const port = process.env.PORT || 4000;
+
+app.use(morgan("common"));
+app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "You have request the / path",
+  });
+});
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
